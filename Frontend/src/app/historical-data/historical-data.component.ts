28bfbcb83b30d5b80a4671bc2e0d8b7bee578e3d
RUHAN VENUJA NANAYAKKARA <103522565@student.swin.edu.au>
2023-09-24 00:14:17 +1000

    Search functionality reworked.

--- Frontend/src/app/historical-data/historical-data.component.ts
+++ Frontend/src/app/historical-data/historical-data.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ColumnMode } from '@swimlane/ngx-datatable';
+
+const days = [ "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" ];
+const months = [ "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" ];
 
 @Component({
   selector: 'app-historical-data',
   templateUrl: './historical-data.component.html',
   styleUrls: ['./historical-data.component.css']
 })
+
 export class HistoricalDataComponent {
+
+  constructor(private router: Router) {}
 
   ColumnMode = ColumnMode;
 
@@ -21,7 +28,7 @@ export class HistoricalDataComponent {
     {
       id: "2",
       image: "url",
-      datetime: new Date("11/09/2023 02:29:45"),
+      datetime: new Date("11/12/2023 02:29:45"),
       verdict: "deceptive",
       confidence: "30%"
     }
@@ -29,6 +36,33 @@ export class HistoricalDataComponent {
 
   searchText="";
 
+  tempRows = this.rows
+
+  onClick() {
+    this.router.navigate(['../current-target']);
+  }
+
   filter() {
+    this.searchText = this.searchText.trim(); // removing leading and trailing whitespaces
+    this.searchText = this.searchText.toLowerCase(); // make searched text lowercase
+    // if no search text display all data
+    if (this.searchText == "")
+      this.tempRows = this.rows;
+    else {
+      this.tempRows = []; // empty temp rows array
+      
+      // filter data
+      this.rows.forEach(row => {
+        if (row.id.includes(this.searchText)
+          || row.datetime.toLocaleDateString().toLowerCase().includes(this.searchText)
+          || row.datetime.toLocaleTimeString().toLowerCase().includes(this.searchText)
+          || days[row.datetime.getDay()].includes(this.searchText)
+          || months[row.datetime.getMonth()].includes(this.searchText)
+          || row.confidence.includes(this.searchText)
+          || row.verdict.toLowerCase().includes(this.searchText)
+        )
+          this.tempRows.push(row); // push matched data into tempRows
+      });
+    }
   }
 }
