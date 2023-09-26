8c6a8cfdba6db82c18e8238775edb963404b6d64
RUHAN VENUJA NANAYAKKARA <103522565@student.swin.edu.au>
2023-09-26 19:24:53 +1000

    Verdict component added.

---
+++ Frontend/src/app/verdict/verdict.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verdict',
+  templateUrl: './verdict.component.html',
+  styleUrls: ['./verdict.component.css']
+})
+export class VerdictComponent {
+
+}
