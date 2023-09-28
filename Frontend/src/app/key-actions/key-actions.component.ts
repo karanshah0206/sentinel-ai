c6cc881ef5f1b73f83151ad9f9e40ef8386f1bd5
RUHAN VENUJA NANAYAKKARA <103522565@student.swin.edu.au>
2023-09-28 15:22:30 +1000

    JSON data retrieval setup for key frames and actions.

--- Frontend/src/app/key-actions/key-actions.component.ts
+++ Frontend/src/app/key-actions/key-actions.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+const URL = "https://localhost:7251/KeyActions";
+
 @Component({
   selector: 'app-key-actions',
   templateUrl: './key-actions.component.html',
@@ -10,4 +12,22 @@ export class KeyActionsComponent {
    "key action", "key action", "key action", "key action", "key action", "key action", 
    "key action", "key action", "key action", "key action", "key action", "key action"];
 
+  loadingResult = true;
+
+  ngOnInit() {
+    fetch(URL).then((fetchedResult) => {
+      fetchedResult.json().then((jsonResult) => {
+        this.keyActions = jsonResult;
+        this.loadingResult = false;
+      }).catch(_ => {
+        // alert("Error occured while parsing data.");
+        this.loadingResult = false;
+        console.error("Error occured while parsing data.");
+      });
+    }).catch((e) => {
+      // alert("Could not fetch data from the server.");
+      this.loadingResult = false;
+      console.error("Could not fetch data from the server.");
+    });
+  }
 }
--- Frontend/src/app/key-frames/key-frames.component.ts
+++ Frontend/src/app/key-frames/key-frames.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
+const URL = "https://localhost:7251/KeyActions";
+
 @Component({
   selector: 'app-key-frames',
   templateUrl: './key-frames.component.html',
@@ -8,8 +10,27 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class KeyFramesComponent {
 
+
   keyFrames = [ "key frame", "key frame", "key frame", "key frame", "key frame",
    "key frame", "key frame", "key frame", "key frame", "key frame", "key frame", 
    "key frame", "key frame", "key frame", "key frame", "key frame", "key frame"];
 
+  loadingResult = true;
+
+  ngOnInit() {
+    fetch(URL).then((fetchedResult) => {
+      fetchedResult.json().then((jsonResult) => {
+        this.keyFrames = jsonResult;
+        this.loadingResult = false;
+      }).catch(_ => {
+        // alert("Error occured while parsing data.");
+        this.loadingResult = false;
+        console.error("Error occured while parsing data.");
+      });
+    }).catch((e) => {
+      // alert("Could not fetch data from the server.");
+      this.loadingResult = false;
+      console.error("Could not fetch data from the server.");
+    });
+  }
 }
