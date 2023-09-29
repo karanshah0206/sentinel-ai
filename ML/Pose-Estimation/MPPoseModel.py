cc77d379ef0cdafe84ba79052c009a2a5bf4ac40
Aid-k <aidynky99@gmail.com>
2023-09-29 17:17:01 +1000

    feat(SEP-10): Simultaneous streaming of raw and processed footage

--- ML/Pose-Estimation/MPPoseModel.py
+++ ML/Pose-Estimation/MPPoseModel.py
@@ -3,11 +3,8 @@ import requests
 import base64
 import mediapipe as mp
 import numpy as np
-import pickle
-import struct
 from mediapipe.python.solutions.pose import PoseLandmark
 from mediapipe.python.solutions.drawing_utils import DrawingSpec
-from server import setup_socket_connection, send_message
 
 pose_estimation_state = False # True when target detected
 
@@ -21,7 +18,6 @@ RIGHT_IRIS = [469, 470, 471, 472]
 
 FACE_OVAL = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 
              149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109]
-
 
 # Define Nose and Mouth connections
 NOSE = [0, 4, 6]
@@ -47,6 +43,9 @@ hand_connections_style = DrawingSpec(color=(0, 255, 0), thickness=2)
 
 excluded_pose_landmarks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22, 25, 26, 27, 28, 29, 30, 31, 32]
 
+holistic = mp_hol.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
 for landmark in excluded_pose_landmarks:
     # Change the way the excluded landmarks are drawn
     custom_style[landmark] = DrawingSpec(color=(255,255,0), thickness=None) 
@@ -60,85 +59,72 @@ def switch_state(image):
     if pose_estimation_state:
         _, img_encoded = cv2.imencode('.jpg', image)
         data = { "Image": str(base64.b64encode(img_encoded))[2:-1] }
-        result = requests.post("http://localhost:5083/History", verify=False, json=data)
-        print(result)
+        # result = requests.post("http://localhost:5083/History", verify=False, json=data)
+        # print(result)
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+def draw(frame):
+    # Detection
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    img_h, img_w = frame.shape[:2]
+    results = holistic.process(image)
+    face_results = face_mesh.process(image)
 
-with mp_hol.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    with mp_face_mesh.FaceMesh(max_num_faces=1, 
-                               refine_landmarks=True, 
-                               min_detection_confidence=0.5,
-                               min_tracking_confidence=0.5) as face_mesh:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            # Detection
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img_h, img_w = frame.shape[:2]
-            results = holistic.process(image)
-            face_results = face_mesh.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # Pose Estimation State Transitions
+    if (face_results.multi_face_landmarks or results.pose_landmarks) and not pose_estimation_state:
+        switch_state(image)
+    elif not (face_results.multi_face_landmarks or results.pose_landmarks) and pose_estimation_state:
+        switch_state(image)
 
-            # Pose Estimation State Transitions
-            if (face_results.multi_face_landmarks or results.pose_landmarks) and not pose_estimation_state:
-                switch_state(image)
-            elif not (face_results.multi_face_landmarks or results.pose_landmarks) and pose_estimation_state:
-                switch_state(image)
+    # Drawing Face, Nose and Mouth Connections
+    if face_results.multi_face_landmarks:
+        for face_landmarks in face_results.multi_face_landmarks:
+            for landmark_num in FACE_OVAL:
+                landmark = face_landmarks.landmark[landmark_num]
+                cv2.circle(image, (int(landmark.x * img_w), int(landmark.y * img_h)), 4, (0, 0, 255), -1)
+            for connection in FACE_OVAL_CONNECTIONS:
+                start = face_landmarks.landmark[connection[0]]
+                end = face_landmarks.landmark[connection[1]]
+                cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
+            
+            for connection in NOSE_CONNECTIONS:
+                start = face_landmarks.landmark[connection[0]]
+                end = face_landmarks.landmark[connection[1]]
+                cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
+            
+            for connection in MOUTH_OUTER_CONNECTIONS:
+                start = face_landmarks.landmark[connection[0]]
+                end = face_landmarks.landmark[connection[1]]
+                cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
+                
+    # Draw hand pose landmarks
+    mp_draw.draw_landmarks(image, results.right_hand_landmarks, mp_hol.HAND_CONNECTIONS, connection_drawing_spec=hand_connections_style)
+    mp_draw.draw_landmarks(image, results.left_hand_landmarks, mp_hol.HAND_CONNECTIONS, connection_drawing_spec=hand_connections_style)
 
-            # Drawing Face, Nose and Mouth Connections
-            if face_results.multi_face_landmarks:
-                for face_landmarks in face_results.multi_face_landmarks:
-                    for landmark_num in FACE_OVAL:
-                        landmark = face_landmarks.landmark[landmark_num]
-                        cv2.circle(image, (int(landmark.x * img_w), int(landmark.y * img_h)), 4, (0, 0, 255), -1)
-                    for connection in FACE_OVAL_CONNECTIONS:
-                        start = face_landmarks.landmark[connection[0]]
-                        end = face_landmarks.landmark[connection[1]]
-                        cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
-                    
-                    for connection in NOSE_CONNECTIONS:
-                        start = face_landmarks.landmark[connection[0]]
-                        end = face_landmarks.landmark[connection[1]]
-                        cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
-                    
-                    for connection in MOUTH_OUTER_CONNECTIONS:
-                        start = face_landmarks.landmark[connection[0]]
-                        end = face_landmarks.landmark[connection[1]]
-                        cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
-                        
-            # Draw hand pose landmarks
-            mp_draw.draw_landmarks(image, results.right_hand_landmarks, mp_hol.HAND_CONNECTIONS, connection_drawing_spec=hand_connections_style)
-            mp_draw.draw_landmarks(image, results.left_hand_landmarks, mp_hol.HAND_CONNECTIONS, connection_drawing_spec=hand_connections_style)
+    # Draw pose landmarks manually
+    if results.pose_landmarks:
+        for idx, landmark in enumerate(results.pose_landmarks.landmark):
+            if landmark.visibility > 0.5:  # Only draw visible landmarks
+                if PoseLandmark(idx) not in excluded_pose_landmarks:
+                    cv2.circle(image, (int(landmark.x * img_w), int(landmark.y * img_h)), 5, (0, 0, 255), -1)
+        for connection in custom_connections:
+            start = results.pose_landmarks.landmark[connection[0]]
+            end = results.pose_landmarks.landmark[connection[1]]
+            if start.visibility > 0.5 and end.visibility > 0.5:  # Only draw visible connections
+                if PoseLandmark(connection[0]) not in excluded_pose_landmarks and PoseLandmark(connection[1]) not in excluded_pose_landmarks:
+                    cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
 
-            # Draw pose landmarks manually
-            if results.pose_landmarks:
-                for idx, landmark in enumerate(results.pose_landmarks.landmark):
-                    if landmark.visibility > 0.5:  # Only draw visible landmarks
-                        if PoseLandmark(idx) not in excluded_pose_landmarks:
-                            cv2.circle(image, (int(landmark.x * img_w), int(landmark.y * img_h)), 5, (0, 0, 255), -1)
-                for connection in custom_connections:
-                    start = results.pose_landmarks.landmark[connection[0]]
-                    end = results.pose_landmarks.landmark[connection[1]]
-                    if start.visibility > 0.5 and end.visibility > 0.5:  # Only draw visible connections
-                        if PoseLandmark(connection[0]) not in excluded_pose_landmarks and PoseLandmark(connection[1]) not in excluded_pose_landmarks:
-                            cv2.line(image, (int(start.x * img_w), int(start.y * img_h)), (int(end.x * img_w), int(end.y * img_h)), (0, 255, 0), 2)
+    if face_results.multi_face_landmarks:
+        mesh_points=np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(int) for p in face_results.multi_face_landmarks[0].landmark])
+        (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(mesh_points[LEFT_IRIS])
+        (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[RIGHT_IRIS])
 
-            if face_results.multi_face_landmarks:
-                mesh_points=np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(int) for p in face_results.multi_face_landmarks[0].landmark])
-                (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(mesh_points[LEFT_IRIS])
-                (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[RIGHT_IRIS])
+        center_left = np.array([l_cx, l_cy], dtype=np.int32)
+        center_right = np.array([r_cx, r_cy], dtype=np.int32)
 
-                center_left = np.array([l_cx, l_cy], dtype=np.int32)
-                center_right = np.array([r_cx, r_cy], dtype=np.int32)
-
-                # Draw the circles with the scaled radius
-                cv2.circle(image, center_left, int(l_radius * scale_factor), (0,255,0), 1, cv2.LINE_AA)
-                cv2.circle(image, center_right, int(r_radius * scale_factor), (0,255,0), 1, cv2.LINE_AA)
-
-                cv2.imshow('Video Feed', image)
-
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-cap.release()
-cv2.destroyAllWindows()
+        # Draw the circles with the scaled radius
+        cv2.circle(image, center_left, int(l_radius * scale_factor), (0,255,0), 1, cv2.LINE_AA)
+        cv2.circle(image, center_right, int(r_radius * scale_factor), (0,255,0), 1, cv2.LINE_AA)
+    
+    return image
--- ML/Pose-Estimation/server.py
+++ ML/Pose-Estimation/server.py
@@ -1,4 +1,4 @@
-import socket, cv2, pickle, struct, imutils
+import socket, pickle, struct, imutils
 
 # Automatic retrieval of host IP Address:
 # host_name  = socket.gethostname()
@@ -6,9 +6,24 @@ import socket, cv2, pickle, struct, imutils
 
 # Hardcoded IP address
 host_ip = '192.168.1.109'
-port = 9999
 
-def setup_socket_connection(host_ip, port):
+# Ports
+raw_video_port = 9999
+processed_video_port = 9998
+
+def setup_server():
+	raw_video_server_socket =  setup_socket(raw_video_port)
+	processed_video_server_socket = setup_socket(processed_video_port)
+
+	# Socket Accept
+	raw_video_client_socket, addr = raw_video_server_socket.accept()
+	print('RECEIVED CONNECTION FROM:', addr)
+	processed_video_client_socket, addr = processed_video_server_socket.accept()
+	print('RECEIVED CONNECTION FROM:', addr)
+	
+	return (raw_video_client_socket, processed_video_client_socket)
+
+def setup_socket(port):
 	# Socket Create
 	server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -24,31 +39,15 @@ def setup_socket_connection(host_ip, port):
 
 	return server_socket
 
-def send_message(server_socket):
-	# Socket Accept
-	while True:
-		client_socket,addr = server_socket.accept()
-		print('GOT CONNECTION FROM:',addr)
-		if client_socket:
-			vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+def send_message(img, client_socket):
+	if client_socket:
+			img - imutils.resize(img, width=640)
+
+			# Serializes a python object into a bytes object
+			a = pickle.dumps(img)
 			
-			while(vid.isOpened()):
-				img, frame = vid.read()
-				frame = imutils.resize(frame, width=480)
-				
-				# Serializes a python object into a bytes object
-				a = pickle.dumps(frame)
-				
-				# Returns a buffer of bytes containing value of 2nd arg
-				message = struct.pack("Q", len(a)) + a
+			# Returns a buffer of bytes containing value of 2nd arg
+			message = struct.pack("Q", len(a)) + a
 
-				# Sends buffer until an exception/error is encountered
-				client_socket.sendall(message)
-				
-				cv2.imshow('Server video feed',frame)
-				key = cv2.waitKey(1) & 0xFF
-				if key ==ord('q'):
-					client_socket.close()
-
-server_socket = setup_socket_connection(host_ip, port)
-send_message(server_socket)
+			# Sends buffer until an exception/error is encountered
+			client_socket.sendall(message)
