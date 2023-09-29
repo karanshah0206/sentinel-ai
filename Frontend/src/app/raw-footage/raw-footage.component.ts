2a9299cb7ed6646088a4c7169940b6f7b78a8521
Trixter36 <mortaza.moh7@gmail.com>
2023-09-29 20:37:19 +1000

    feat: connect to websocket

--- Frontend/src/app/raw-footage/raw-footage.component.ts
+++ Frontend/src/app/raw-footage/raw-footage.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { webSocket } from "rxjs/webSocket";
+
 
 @Component({
   selector: 'app-raw-footage',
   templateUrl: './raw-footage.component.html',
   styleUrls: ['./raw-footage.component.css']
 })
-export class RawFootageComponent {
+export class RawFootageComponent implements OnInit {
+  @ViewChild('videoPlayer') videoPlayer: ElementRef;
+
+  constructor() {}
+  
+  subject = webSocket("192.168.1.109:9999");
+
+  ngOnInit() {
+    this.subject.subscribe((stream) => {
+      console.log(stream);
+
+      const videoPlayer = this.videoPlayer.nativeElement;
+      videoPlayer.src = stream;
+
+      videoPlayer.play();
+    })
+  }
 }
